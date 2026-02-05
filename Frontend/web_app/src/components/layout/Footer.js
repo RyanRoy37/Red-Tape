304bfc722d9a7d27db0a2c0694e744fb82a6ebff
RyanRoy37 <ryanrj9341@gmail.com>
2026-02-05 12:21:58 +0530

    Updated Github ID

--- Frontend/web_app/src/components/layout/Footer.js
+++ Frontend/web_app/src/components/layout/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="text-center mt-24 pb-8 opacity-60">
       <p className="text-sm tracking-widest">
-        [ BUILT BY @J RYAN ROY <a href="jryanroy.in"><u>jryanroy.in</u></a> & AKANKSHA SINGH <a href="https://github.com/Akanksha2121"><u>akanksha2121</u></a>  ].
+        [ BUILT BY @J RYAN ROY <a href="jryanroy.in"><u>jryanroy.in</u></a> & AKANKSHA SINGH <a href="https://github.com/Akanksha2124"><u>akanksha2121</u></a>  ].
       </p>
       < p className="text-sm tracking-widest mt-1">
       This UI was built with the help of Claude </p>
